002a0ea6c27cfbfe82abb306a0fd0dd3d7dc051e
Eric Klassen <30785238+eklass3@users.noreply.github.com>
2024-04-09 21:54:54 +0200

    Added button to header for "About Us". Changed title to business name.

--- capp/__init__.py
+++ capp/__init__.py
@@ -4,16 +4,17 @@ import os
 application = Flask(__name__)
 
 # application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-
 application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
 from capp.carbon_app.routes import carbon_app
 from capp.users.routes import users
+from capp.about_us.routes import about_us
 
 application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_app)
 application.register_blueprint(users)
+application.register_blueprint(about_us)
 
